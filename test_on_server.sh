d9f29b2b1f9640f642c69790aa38e4a8cd90eda5
Darwin <darwin.git@marianna.se>
2011-11-21 16:06:48 +0100

    added useragent to the gtk version

--- test_on_server.sh
+++ test_on_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cmd='cd /tmp/; chmod +rx webkit2png;xvfb-run --server-args="-screen 0, 1024x768x24" ./webkit2png --filename=test.png --user-agent="dummy" -F --delay=3 '
+cmd='cd /tmp/; chmod +rx webkit2png;xvfb-run --server-args="-screen 0, 1024x768x24" ./webkit2png --filename=test.png --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/534.51.22 (KHTML, like Gecko) Version/5.1.1 Safari/534.51.22" -F --delay=3 '
 cmd=$cmd$2
 
 
